d8d7097f248f36a5a31a15338bb52373a3cdeefb
kasi963 <raahaadam8@gmail.com>
2025-05-28 09:02:00 +0000

    Update coding.py

--- coding.py
+++ coding.py
@@ -5,3 +5,5 @@ days_age=365*new_age
 weeks_age=new_age*52
 months_age=new_age*12
 print("you have " + str(days_age) + " days and\n " + str(weeks_age) + " weeks and\n" + str(months_age)+" months left")
+
+this code tells you the number of days,weeks and months you have left to live assuming you were to live to 90 years
